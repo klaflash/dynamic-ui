a2b18c2af5dd9d177d259b6f279da39ef5cba406
Kyle <klaflash@purdue.edu>
2022-08-01 19:29:26 -0400

    Added tabbed navigation

---
+++ mobile/script.js
@@ -0,0 +1,64 @@
+const display = () => {
+
+    const home = document.querySelector('.home');
+    const homePage = document.querySelector('.home-page');
+    const payments = document.querySelector('.payments');
+    const paymentsPage = document.querySelector('.payments-page');
+    const add = document.querySelector('.add');
+    const addPage = document.querySelector('.add-page');
+    const wallet = document.querySelector('.wallet');
+    const walletPage = document.querySelector('.wallet-page');
+    const scan = document.querySelector('.scan');
+    const scanPage = document.querySelector('.scan-page');
+
+    homePage.classList.add('display-page');
+
+    home.addEventListener('click', () => {
+        paymentsPage.classList.remove('display-page');
+        addPage.classList.remove('display-page');
+        walletPage.classList.remove('display-page');
+        scanPage.classList.remove('display-page');
+
+        homePage.classList.add('display-page');
+    });
+
+    payments.addEventListener('click', () => {
+        addPage.classList.remove('display-page');
+        walletPage.classList.remove('display-page');
+        scanPage.classList.remove('display-page');
+        homePage.classList.remove('display-page');
+
+        paymentsPage.classList.add('display-page');
+    });
+
+    add.addEventListener('click', () => {
+        walletPage.classList.remove('display-page');
+        scanPage.classList.remove('display-page');
+        homePage.classList.remove('display-page');
+        paymentsPage.classList.remove('display-page');
+
+        addPage.classList.add('display-page');
+    });
+
+    wallet.addEventListener('click', () => {
+        addPage.classList.remove('display-page');
+        scanPage.classList.remove('display-page');
+        homePage.classList.remove('display-page');
+        paymentsPage.classList.remove('display-page');
+
+        walletPage.classList.add('display-page');
+    });
+
+    scan.addEventListener('click', () => {
+        walletPage.classList.remove('display-page');
+        addPage.classList.remove('display-page');
+        homePage.classList.remove('display-page');
+        paymentsPage.classList.remove('display-page');
+
+        scanPage.classList.add('display-page');
+    });
+
+
+};
+
+display();
